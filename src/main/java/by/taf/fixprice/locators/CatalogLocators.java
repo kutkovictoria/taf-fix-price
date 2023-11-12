ed0200a631b9bdf62ec722b7fdc5a6de626df164
Viktoryia Kutko <kutkovictoria@gmail.com>
2023-11-12 15:15:51 +0300

    add CatalogLocators, refactor CatalogPage

---
+++ src/main/java/by/taf/fixprice/locators/CatalogLocators.java
@@ -0,0 +1,8 @@
+package by.taf.fixprice.locators;
+
+public class CatalogLocators {
+    public static final String OFFICE_SUPPLIES_LOCATOR = "//a[@href='/catalog/kantstovary']";
+    public static final String BERLINGO_CHECKBOX_LOCATOR = "//input[@id='brand_15848']";
+    public static final String PRICE_3BYN_LOCATOR = "//input[@id='price_110000300']";
+
+}
--- src/main/java/by/taf/fixprice/po/CatalogPage.java
+++ src/main/java/by/taf/fixprice/po/CatalogPage.java
@@ -1,4 +1,17 @@
 package by.taf.fixprice.po;
 
+import by.taf.fixprice.webdriver.Singleton;
+
+import static by.taf.fixprice.locators.CatalogLocators.*;
+
 public class CatalogPage {
+    public void clickOfficeSuppliesLink(){
+        Singleton.clickWebElement(OFFICE_SUPPLIES_LOCATOR);
+    }
+    public void clickBerlingoCheckBox(){
+        Singleton.clickWebElement(BERLINGO_CHECKBOX_LOCATOR);
+    }
+    public void clickPrice3BYN(){
+        Singleton.clickWebElement(PRICE_3BYN_LOCATOR);
+    }
 }
